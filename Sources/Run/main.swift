7be054c8fbf42ebf8e9f11a49552e7a8273e3388
Luke Stringer <luke@lukestringer.me>
2018-06-01 19:13:16 +0100

    Add Sword dependency

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -1,25 +1,17 @@
 import App
+import Sword
 
-/// We have isolated all of our App's logic into
-/// the App module because it makes our app
-/// more testable.
-///
-/// In general, the executable portion of our App
-/// shouldn't include much more code than is presented
-/// here.
-///
-/// We simply initialize our Droplet, optionally
-/// passing in values if necessary
-/// Then, we pass it to our App's setup function
-/// this should setup all the routes and special
-/// features of our app
-///
-/// .run() runs the Droplet's commands, 
-/// if no command is given, it will default to "serve"
-let config = try Config()
-try config.setup()
 
-let drop = try Droplet(config)
-try drop.setup()
+let bot = Sword(token: "NDUwMzk0OTMwMDYyNDI2MTEy.DfMVYw.AybPKPH18cu1N9BBTvOtr1jqlfw")
 
-try drop.run()
+bot.editStatus(to: "online", playing: "Testing!")
+
+bot.on(.messageCreate) { data in
+    let msg = data as! Message
+    
+    if msg.content == "!ping" {
+        msg.reply(with: "Pong!")
+    }
+}
+
+bot.connect()
