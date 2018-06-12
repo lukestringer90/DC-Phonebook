c1a6fa675d00963dc4bee17d61e9aee8cd19020a
Luke Stringer <luke@lukestringer.me>
2018-06-12 21:04:21 +0100

    Refactor type locations

--- Sources/App/Discord/OnMessageController.swift
+++ Sources/App/Discord/OnMessageController.swift
@@ -68,25 +68,3 @@ extension Message {
         }
     }
 }
-
-extension Sword: SendMessage {
-    func send(_ messageText: String, to userID: SnowflakeID, withEmojiReactions reactions: [EmojiReaction]?) {
-        send(messageText, to: Snowflake(rawValue: userID)) { message, error in
-            guard let reactions = reactions, let message = message else { return }
-            
-            for reaction in reactions {
-                self.addReaction(reaction.rawValue, to: message.id, in: Snowflake(rawValue: userID), then: { reactionError in
-                    print(reactionError)
-                })
-            }
-        }
-    }
-    
-    func send(_ messageText: String, to userID: SnowflakeID) {
-        send(messageText, to: Snowflake(rawValue: userID)) { message, error in
-            print(message)
-            print(error)
-        }
-    }
-}
-
