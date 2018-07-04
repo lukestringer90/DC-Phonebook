e73c3cf842f59e4d8d58a5975d6cafc9e085f704
Luke Stringer <luke@lukestringer.me>
2018-07-04 19:54:21 +0100

    Begin conversion to CLI app

--- DC-Bot/Package.swift
+++ DC-Bot/Package.swift
@@ -6,15 +6,13 @@ import PackageDescription
 let package = Package(
     name: "DC-Bot",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.4"),
+        .package(url: "https://github.com/Azoy/Sword.git", from: "0.9.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "DC-Bot",
-            dependencies: ["DC-BotCore"]),
-        .target(name: "DC-BotCore")
+//            dependencies: ["Sword"])
+            dependencies: ["FluentSQLite", "Sword"])
     ]
 )
--- DC-Bot/Sources/DC-Bot/main.swift
+++ DC-Bot/Sources/DC-Bot/main.swift
@@ -1,8 +1,31 @@
+import Foundation
+import Sword
 import DC_BotCore
 
-let tool = CommandLineTool()
-do {
-    try tool.run()
-} catch {
-    print("Whoops! An error occurred: \(error)")
-}
+let discordConfigFileName = "discord"
+let botTokenKey = "discordBotToken"
+
+//guard let token = ProcessInfo.processInfo.environment[botTokenKey] else {
+//    fatalError("No \(botTokenKey) env var")
+//}
+
+let token = "NDUwMzk0OTMwMDYyNDI2MTEy.DhwRgg.pAeg8PIXalSM9b3OB_PGfjbFX2k"
+
+let config = DiscordConfig(channelIDs: DiscordConfig.ChannelIDs(phoneBookRequests: UInt64(450397327295905803), phoneBookDirectory: UInt64(450397213319757854), logs: UInt64(450397168046440449)), roleIDs: DiscordConfig.RoleIDs(verified: 455104920673058817), verifyStartMessage: DiscordConfig.VerifyStartMessage(command: "!verify", secondsBeforeDeletion: 7.0))
+
+var options = SwordOptions()
+options.willCacheAllMembers = true
+let bot = Sword(token: token, with: options)
+
+let onMessageController = OnMessageController(discord: bot, config: config)
+let onReactionAddController = OnReactionAddController(discord: bot, config: config)
+
+bot.editStatus(to: "online", playing: "In Development")
+bot.on(.messageCreate, do: onMessageController.handle)
+bot.on(.reactionAdd, do: onReactionAddController.handle)
+bot.connect()
+
+let runLoop = RunLoop.current
+let distantFuture = Date.distantFuture
+
+runLoop.run(mode: .defaultRunLoopMode, before: distantFuture)
