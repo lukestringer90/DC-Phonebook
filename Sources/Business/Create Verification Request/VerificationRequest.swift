2ecd1a4aaf82d480ad01a0b6598ed7a3c50c2860
Luke Stringer <luke@lukestringer.me>
2018-08-12 19:21:41 +0100

    Remove unnecessary todos

--- Sources/Business/Create Verification Request/VerificationRequest.swift
+++ Sources/Business/Create Verification Request/VerificationRequest.swift
@@ -53,7 +53,6 @@ class VerificationRequest: Storable {
 	static func prepare(_ database: Database) throws {
 		try database.create(self) { signals in
 			signals.id()
-			// TODO: Make sure these work for UInt64
 			signals.int("userID")
 			signals.string("scrollURL")
 			signals.string("forumPage")
