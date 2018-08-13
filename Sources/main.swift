121de325f53c8b316e89ee734e140c061b15c6f0
Luke Stringer <luke@lukestringer.me>
2018-08-13 21:36:25 +0100

    Cleanup setup

--- Sources/main.swift
+++ Sources/main.swift
@@ -3,6 +3,8 @@ import Sword
 import PostgreSQLDriver
 import FluentProvider
 
+// MARK: - Get env vars
+
 let flavourKey = "flavour"
 let botTokenKey = "discordBotToken"
 
@@ -14,42 +16,60 @@ guard let flavour = ProcessInfo.processInfo.environment[flavourKey] else {
 	fataError_flush("No \(flavourKey) env var")
 }
 
-let driver: PostgreSQLDriver.Driver = {
-	if let dbURL = ProcessInfo.processInfo.environment["DATABASE_URL"] {
-		do {
-			print_flush("Using env var for DB URL: \(dbURL)")
-			fflush(stdout)
-			return try PostgreSQLDriver.Driver(url: dbURL)
+print_flush("Running as \(flavour) flavour")
+
+// MARK: - Database setup
+
+func setupDatabase(){
+	let driver: PostgreSQLDriver.Driver = {
+		if let dbURL = ProcessInfo.processInfo.environment["DATABASE_URL"] {
+			do {
+				print_flush("Using env var for DB URL: \(dbURL)")
+				fflush(stdout)
+				return try PostgreSQLDriver.Driver(url: dbURL)
+			}
+			catch {
+				fataError_flush(error)
+			}
 		}
-		catch {
-			fataError_flush(error)
-		}
-	}
-	print_flush("Using localhost for DB URL")
-	fflush(stdout)
-	return try! PostgreSQLDriver.Driver(masterHostname: "localhost", readReplicaHostnames: [], user: "", password: "", database: "luke")
-}()
-let database = Database(driver)
-try! database.prepare([VerifyStartSignal.self, VerificationRequest.self])
+		print_flush("Using localhost for DB URL")
+		fflush(stdout)
+		return try! PostgreSQLDriver.Driver(masterHostname: "localhost", readReplicaHostnames: [], user: "", password: "", database: "luke")
+	}()
+	let database = Database(driver)
+	try! database.prepare([VerifyStartSignal.self, VerificationRequest.self])
+}
 
-let bytes = try! DataFile(workDir: "").read(at: "Config/Discord/\(flavour).json")
-let data = Data(bytes: bytes)
+// MARK: - Discor Bot setup
 
-let config = try! JSONDecoder().decode(DiscordConfig.self, from: data)
+func setupBot(token: String, config: DiscordConfig) {
+	var options = SwordOptions()
+	options.willCacheAllMembers = true
+	let bot = Sword(token: token, with: options)
+	
+	let onMessageController = OnMessageController(discord: bot, config: config)
+	let onReactionAddController = OnReactionAddController(discord: bot, config: config)
+	
+	bot.editStatus(to: "online", playing: "In Development")
+	bot.on(.messageCreate, do: onMessageController.handle)
+	bot.on(.reactionAdd, do: onReactionAddController.handle)
+	bot.connect()
+}
 
-var options = SwordOptions()
-options.willCacheAllMembers = true
-let bot = Sword(token: token, with: options)
+// MARK: - Load flavour config
 
-let onMessageController = OnMessageController(discord: bot, config: config)
-let onReactionAddController = OnReactionAddController(discord: bot, config: config)
+func loadConfig(from flavour: String) -> DiscordConfig {
+	
+	let bytes = try! DataFile(workDir: "").read(at: "Config/Discord/\(flavour).json")
+	let data = Data(bytes: bytes)
+	return try! JSONDecoder().decode(DiscordConfig.self, from: data)
+}
 
-bot.editStatus(to: "online", playing: "In Development")
-bot.on(.messageCreate, do: onMessageController.handle)
-bot.on(.reactionAdd, do: onReactionAddController.handle)
-bot.connect()
+// MARK: - Run setup processes
 
-let runLoop = RunLoop.current
-let distantFuture = Date.distantFuture
+setupDatabase()
+let config = loadConfig(from: flavour)
+setupBot(token: token, config: config)
 
-runLoop.run(mode: .defaultRunLoopMode, before: distantFuture)
+RunLoop.current.run(mode: .defaultRunLoopMode, before: Date.distantFuture)
+
